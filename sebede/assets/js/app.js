d5bbc6382ae3f759a478a0649419af0664f91953
AdavizeTheFirst <42492202+AdavizeTheFirst@users.noreply.github.com>
2020-06-28 01:57:47 +0100

    Add files via upload

--- sebede/assets/js/app.js
+++ sebede/assets/js/app.js
@@ -1,4 +1,5 @@
-var notify = `			<div id="notification-container" class="col">
+var notify = `
+<div id="notification-container" class="col">
 <div class="notification-wrapper col">
  <img src="./assets/img/success.svg" alt="success symbol">
  <h1 class="transaction-status">Transaction successful</h1>
@@ -10,12 +11,25 @@ var notify = `			<div id="notification-container" class="col">
  <h2>Other services <a href="tel:+2348144935610">( 081 4493 5610 )</a></h2>
  <li>Sales of PIN for result (WAEC, NECO, GCE, JAMB)</li>
  <li>Cable TV subscription (DsTv, GoTv, Startimes)</li>
- <li>Cheap bulk airtime sales</li>
- <li>Cheap bulk data sales</li>
+ <li>Cheap bulk airtime/data sales</li>
+ <li>Mini Importation coach</li>
  <li>Airtime to cash</li>
 </ul>
 </div>`
+var ad
+var main = document.getElementById("main")
 document.getElementById("proceed-btn").addEventListener("click", function(e){
- e.preventDefault()
-document.getElementById("main").innerHTML = notify;
+e.preventDefault()
+main.innerHTML = notify;
+var adContent = document.createElement("div");
+adContent.id = "ad";
+adContent.innerHTML = `
+<div class="" id="ad-card">
+  <button id="cancel-ad"><img src="./assets/img/cancel-ad.svg" alt="cancel button icon"></button>
+</div>`
+main.appendChild(adContent)
+ad = document.getElementById("ad");
+document.getElementById("cancel-ad").addEventListener("click", function(){
+  ad.remove()
+})
 })
